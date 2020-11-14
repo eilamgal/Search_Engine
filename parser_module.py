3b757afbc7b148bb4276c73672e7a2be96943515
eilam gal <eilamg@post.bgu.ac.il>
2020-11-15 00:23:37 +0200

    "prase hashtags"

--- parser_module.py
+++ parser_module.py
@@ -18,6 +18,37 @@ class Parse:
         text_tokens_without_stopwords = [w.lower() for w in text_tokens if w not in self.stop_words]
         return text_tokens_without_stopwords
 
+    def parse_hashtags(self, text): #problems: USE will translate to  u,s,a and all so
+        list_of_hashtags = []
+        new_full_text = ""
+        tokens_array = text.split(' ')
+        for token in tokens_array:
+            pos = token.find('#')
+            if pos >= 0:
+                hashtag = token[pos:len(token)]
+                if hashtag.find("...https") > 0:
+                    new_full_text = new_full_text + " " + hashtag[hashtag.find("...https"):]
+                    hashtag = hashtag[0:hashtag.find("...https")]
+                # option for two hashtags in a row
+                list = []
+                list.append(hashtag)
+                for str in list:
+                    list_of_hashtags.append(str.lower())
+                    i = 2
+                    pos = 1
+                    while i <= len(str):
+                        if i == len(str) or str[i].isupper() or str[i] == "_":
+                            list_of_hashtags.append(str[pos:i].lower())
+                            if i == len(str) or str[i].isupper():
+                                pos = i
+                            else:
+                                pos = i+1
+                        i += 1
+            else:
+                new_full_text = new_full_text + " " + token
+        return list_of_hashtags, new_full_text
+
+
     def parse_doc(self, doc_as_list):
         """
         This function takes a tweet document as list and break it into different fields
@@ -28,12 +59,21 @@ class Parse:
         tweet_date = doc_as_list[1]
         full_text = doc_as_list[2]
         url = doc_as_list[3]
-        retweet_text = doc_as_list[4]
-        retweet_url = doc_as_list[5]
-        quote_text = doc_as_list[6]
-        quote_url = doc_as_list[7]
+        url_indices = doc_as_list[4]
+        retweet_text = doc_as_list[5]
+        retweet_url = doc_as_list[6]
+        retweet_url_indices = doc_as_list[7]
+        quote_text = doc_as_list[8]
+        quote_url = doc_as_list[9]
+        quote_url_indices = doc_as_list[10]
+        retweet_quoted_text = doc_as_list[11]
+        retweet_quoted_url = doc_as_list[12]
+        retweet_quoted_url_indices = doc_as_list[13]
         term_dict = {}
+        x, y = tokenized = self.parse_hashtags(full_text)
+
         tokenized_text = self.parse_sentence(full_text)
+
 
         doc_length = len(tokenized_text)  # after text operations.
 
