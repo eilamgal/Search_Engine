1b11bf31d7eec110376558c7075b5bc7b51dc416
oded <odedodge@gmail.com>
2020-11-15 20:38:15 +0200

    Parser - Preprocess text function

--- indexer.py
+++ indexer.py
@@ -18,9 +18,20 @@ class Indexer:
         for term in document_dictionary.keys():
             try:
                 # Update inverted index and posting
-                if term not in self.inverted_idx.keys():
+                if term.lower not in self.inverted_idx.keys() and term.upper() not in self.inverted_idx.keys():
                     self.inverted_idx[term] = 1
                     self.postingDict[term] = []
+
+                elif term.isupper() and term.lower in self.inverted_idx.keys():
+                    self.inverted_idx[term.lower] += 1
+                    term = term.lower()
+
+                elif term.islower() and term.upper in self.inverted_idx.keys():
+                    self.inverted_idx[term] = self.inverted_idx[term.upper] + 1
+                    del self.inverted_idx[term.upper]
+                    self.postingDict[term] = self.postingDict[term.upper]
+                    del self.postingDict[term.upper]
+
                 else:
                     self.inverted_idx[term] += 1
 
@@ -28,3 +39,4 @@ class Indexer:
 
             except:
                 print('problem with the following key {}'.format(term[0]))
+
--- parser_module.py
+++ parser_module.py
@@ -8,18 +8,23 @@ def remove_urls(full_text):
         return
     split = full_text.split(' ')
     clean = [token for token in split if "http" not in token.lower()]
-    # for token in split:
-    #     if "http" not in token.lower():
-    #         clean.append(token)
     return ' '.join(clean)
 
 
-def expand_url(url):
+def tokenize_url(url):
     if not url:
         return
     url_split = url.find("\":\"")
+    if url_split == -1:
+        return
     long_url = url[url_split + 3:]
-    return long_url[:len(long_url) - 2]
+    long_url = long_url[:len(long_url) - 2]
+    tokenized_url = long_url.replace('/', '.').replace('-', '.').split('.')
+    return tokenized_url
+
+
+def parse_number(number, param):
+    pass
 
 
 class Parse:
@@ -37,7 +42,7 @@ class Parse:
         text_tokens_without_stopwords = [w.lower() for w in text_tokens if w not in self.stop_words]
         return text_tokens_without_stopwords
 
-    def parse_hashtags(self, text): #problems: USE will translate to  u,s,a and all so
+    def parse_hashtags(self, text):  # problems: USE will translate to  u,s,a and all so
         list_of_hashtags = []
         new_full_text = ""
         tokens_array = text.split(' ')
@@ -61,12 +66,11 @@ class Parse:
                             if i == len(str) or str[i].isupper():
                                 pos = i
                             else:
-                                pos = i+1
+                                pos = i + 1
                         i += 1
             else:
                 new_full_text = new_full_text + " " + token
         return list_of_hashtags, new_full_text
-
 
     def parse_doc(self, doc_as_list):
         """
@@ -76,21 +80,39 @@ class Parse:
         """
         tweet_id = doc_as_list[0]
         tweet_date = doc_as_list[1]
-        full_text = remove_urls(doc_as_list[2])
-        url = expand_url(doc_as_list[3])
+        full_text = doc_as_list[2]
+        url_tokens = tokenize_url(doc_as_list[3])
         # url_indices = doc_as_list[4]
-        retweet_text = remove_urls(doc_as_list[5])
-        retweet_url = expand_url(doc_as_list[6])
+        retweet_text = doc_as_list[5]
+        retweet_url_tokens = tokenize_url(doc_as_list[6])
         # retweet_url_indices = doc_as_list[7]
-        quote_text = remove_urls(doc_as_list[8])
-        quote_url = expand_url(doc_as_list[9])
+        quote_text = doc_as_list[8]
+        quote_url_tokens = tokenize_url(doc_as_list[9])
         quote_url_indices = doc_as_list[10]
-        retweet_quoted_text = remove_urls(doc_as_list[11])
-        retweet_quoted_url = expand_url(doc_as_list[12])
+        retweet_quoted_text = doc_as_list[11]
+        retweet_quoted_url_tokens = tokenize_url(doc_as_list[12])
         # retweet_quoted_url_indices = doc_as_list[13]
         term_dict = {}
-        tokenized_text = self.parse_sentence(full_text)
-        x, y = tokenized = self.parse_hashtags(full_text)
+
+        preprocessed = self.pre_process(full_text)
+        full_text = preprocessed[0]
+        tokenized_text = preprocessed[1]
+
+        preprocessed = self.pre_process(retweet_text)
+        retweet_text = preprocessed[0]
+        tokenized_text.append(preprocessed[1])
+
+        preprocessed = self.pre_process(quote_text)
+        quote_text = preprocessed[0]
+        tokenized_text.append(preprocessed[1])
+
+        tokenized_text.append(self.parse_sentence(full_text))
+        tokenized_text.append(self.parse_sentence(retweet_text))
+        tokenized_text.append(self.parse_sentence(quote_text))
+
+        tokenized_text.append(url_tokens)
+        tokenized_text.append(retweet_url_tokens)
+        tokenized_text.append(quote_url_tokens)
 
         doc_length = len(tokenized_text)  # after text operations.
 
@@ -100,6 +122,50 @@ class Parse:
             else:
                 term_dict[term] += 1
 
-        document = Document(tweet_id, tweet_date, full_text, url, retweet_text, retweet_url, quote_text,
-                            quote_url, term_dict, doc_length)
+        document = Document(tweet_id, tweet_date, full_text, url_tokens, retweet_text, retweet_url_tokens, quote_text,
+                            quote_url_tokens, term_dict, doc_length)
         return document
+
+    def pre_process(self, text):
+        if not text:
+            return
+        tokens_list = []
+        clean_text = []
+        text = text.replace("percentage", "percent")
+        split = text.split(' ')
+        for i in range(len(split)):
+            token = split[i]
+            try:
+                # HASHTAGS
+                if token[0] == '#':
+                    tokens_list.append(self.parse_hashtags(token))
+
+                # TAGS
+                elif token[0] == '@':
+                    tokens_list.append(token)
+
+                # PERCENTAGES
+                elif i < len(split) - 1 and split[i + 1] == "percent":
+                    number = float(token)
+                    if token.isnumeric():
+                        tokens_list.append(token + "%")
+                    else:
+                        tokens_list.append(str(number) + "%")
+                elif token.endswith('%'):
+                    number = float(token[:token.find('%')])
+                    tokens_list.append(token)
+
+                # NUMBERS
+                elif token.replace(',', '').replace('.', '').isnumeric():
+                    number = token.replace(',', '')
+                    if i < len(split) - 1:
+                        tokens_list.append(parse_number(number, split[i + 1]))
+                    else:
+                        tokens_list.append(parse_number(number, ""))
+                # ALL THE REST - REGULAR TOKENS
+                else:
+                    clean_text.append(token)
+            except:
+                clean_text.append(token)
+
+        return ' '.join(clean_text), tokens_list
