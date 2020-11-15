4d3422ef3b1cc7cef68c11633e2f9bbb6af78f27
oded <odedodge@gmail.com>
2020-11-15 14:28:58 +0200

    Parser - Clean text from URLs and expand url methods

--- parser_module.py
+++ parser_module.py
@@ -3,6 +3,25 @@ from nltk.tokenize import word_tokenize
 from document import Document
 
 
+def remove_urls(full_text):
+    if not full_text:
+        return
+    split = full_text.split(' ')
+    clean = [token for token in split if "http" not in token.lower()]
+    # for token in split:
+    #     if "http" not in token.lower():
+    #         clean.append(token)
+    return ' '.join(clean)
+
+
+def expand_url(url):
+    if not url:
+        return
+    url_split = url.find("\":\"")
+    long_url = url[url_split + 3:]
+    return long_url[:len(long_url) - 2]
+
+
 class Parse:
 
     def __init__(self):
@@ -18,22 +37,61 @@ class Parse:
         text_tokens_without_stopwords = [w.lower() for w in text_tokens if w not in self.stop_words]
         return text_tokens_without_stopwords
 
+    # def handle_urls(self, doc):
+    #     """
+    #     This function processes a doc_list, replaces shortened urls with full ones and tokenize them
+    #     :param doc: raw doc_list from file reader
+    #     :return: clean doc_list without unhandled URLs
+    #     """
+    #     full_text = doc[2].split(' ')
+    #     url = doc[3]
+    #     url_indices = doc[4]
+    #     quote_text = []
+    #     quote_url = {}
+    #     quote_url_indices = []
+    #     if doc[8]:
+    #         quote_text = doc[8].split(' ')
+    #         quote_url = doc[9]
+    #         quote_url_indices = doc[10]
+    #     new_text = []
+    #
+    #     for token in full_text:
+    #         new_token = token
+    #         if "http" in token and token in url:
+    #             url_split = url.find("\":\"")
+    #             long_url = url[url_split+3:]
+    #             long_url = long_url[:len(long_url)-2]
+    #             new_token = long_url
+    #         new_text.append(new_token)
+    #
+    #     print(new_text)
+    #     return ""
+
     def parse_doc(self, doc_as_list):
         """
         This function takes a tweet document as list and break it into different fields
         :param doc_as_list: list re-preseting the tweet.
         :return: Document object with corresponding fields.
         """
+        # self.handle_urls(doc_as_list)
         tweet_id = doc_as_list[0]
         tweet_date = doc_as_list[1]
-        full_text = doc_as_list[2]
-        url = doc_as_list[3]
-        retweet_text = doc_as_list[4]
-        retweet_url = doc_as_list[5]
-        quote_text = doc_as_list[6]
-        quote_url = doc_as_list[7]
+        full_text = remove_urls(doc_as_list[2])
+        url = expand_url(doc_as_list[3])
+        # url_indices = doc_as_list[4]
+        retweet_text = remove_urls(doc_as_list[5])
+        retweet_url = expand_url(doc_as_list[6])
+        # retweet_url_indices = doc_as_list[7]
+        quote_text = remove_urls(doc_as_list[8])
+        quote_url = expand_url(doc_as_list[9])
+        quote_url_indices = doc_as_list[10]
+        retweet_quoted_text = remove_urls(doc_as_list[11])
+        retweet_quoted_url = expand_url(doc_as_list[12])
+        # retweet_quoted_url_indices = doc_as_list[13]
         term_dict = {}
+
         tokenized_text = self.parse_sentence(full_text)
+        tokenized_urls = self.parse_sentence(url)
 
         doc_length = len(tokenized_text)  # after text operations.
 
